5a9bb5090f54e9ab61588f5cb7ee717c9ba0be31
Linh Dinh <dinh.thi.khanh.linh@sun-asterisk.com>
2024-08-01 13:41:32 +0700

    Chapter 6

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  before_save :downcase_email
+  after_update :run_callback_after_update
+
+  validates :name, presence: true, length: {maximum: Settings.validate_len_name}
+  validates :email, presence: true,
+length: {maximum: Settings.validate_len_email}
+  # format: {with: Settings.VALID_EMAIL_REGEX}, uniqueness:true
+  has_secure_password
+
+  private
+
+  def run_callback_after_update
+    Rails.logger.debug "callback after update"
+  end
+
+  def downcase_email
+    email.downcase!
+  end
+end
