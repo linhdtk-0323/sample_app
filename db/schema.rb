9e2bbe1c8e9279ee659f1d34b929e6dafcf01727
Linh Dinh <dinh.thi.khanh.linh@sun-asterisk.com>
2024-08-01 13:41:32 +0700

    Chapter 6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_01_084128) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_01_060648) do
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
