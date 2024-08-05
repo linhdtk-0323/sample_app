a9c9ece3e2947818fe79a4f7db83abab566b2e7a
Linh Dinh <dinh.thi.khanh.linh@sun-asterisk.com>
2024-08-05 16:50:51 +0700

    Chapter 9: advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save :downcase_email
-  after_update :run_callback_after_update
+  attr_accessor :remember_token
 
   PERMITTED_ATRIBUTES = %i(name email password password_confirmation).freeze
   validates :name, presence: true, length: {maximum: Settings.validate_len_name}
@@ -16,6 +16,23 @@ class User < ApplicationRecord
              BCrypt::Engine.cost
            end
     BCrypt::Password.create string, cost:
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_column(:remember_digest, nil)
   end
 
   private
