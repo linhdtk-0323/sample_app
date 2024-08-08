91c0b7e2f3e5d84f330461a5a1cf048391fa31fc
Linh Dinh <dinh.thi.khanh.linh@sun-asterisk.com>
2024-08-08 11:05:51 +0700

    Chapter 13: Microposts

---
+++ app/models/micropost.rb
@@ -0,0 +1,15 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image do |attachable|
+    attachable.variant :display, resize_to_limit: Settings.resize_to_limit
+  end
+
+  validates :user_id, presence: true
+  validates :content, presence: true, length: {maximum: Settings.maximum_140}
+  validates :image, content_type: {in: Settings.accept_file,
+                                   message: I18n.t("message_upload")},
+                    size: {less_than: Settings.file_5.megabytes,
+                           message: I18n.t("file_size")}
+
+  scope :recent_posts, ->{order created_at: :desc}
+end
